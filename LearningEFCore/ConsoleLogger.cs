2ff326846854f17c1002f0543614456931d2b246
ar.hafizi <ar.hafizi@OWJ-12>
2023-08-08 16:46:49 +0330

    Add Logger class

---
+++ LearningEFCore/ConsoleLogger.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningEFCore;
+
+public class ConsoleLoggerProvider : ILoggerProvider {
+    public ILogger CreateLogger(string categoryName) {
+        return new ConsoleLogger();
+    }
+
+    public void Dispose() {
+    }
+}
+
+public class ConsoleLogger : ILogger {
+    public IDisposable BeginScope<TState>(TState state) {
+        return null;
+    }
+
+    public bool IsEnabled(LogLevel logLevel) {
+
+        return logLevel switch {
+            LogLevel.Trace or LogLevel.Information or LogLevel.None => false,
+            _ => true,
+        };
+    }
+
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter) {
+
+        Console.Write($"Level: {logLevel}, Event Id: {eventId.Id}");
+
+        if (state != null) {
+            Console.Write($", State: {state}");
+        }
+        if (exception != null) {
+            Console.Write($", Exception: {exception.Message}");
+        }
+        Console.WriteLine();
+    }
+}
